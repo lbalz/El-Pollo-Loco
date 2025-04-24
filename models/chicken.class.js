bd5fa01b1f520f2a017c388ca39946d1365592e6
Lukas Balz <lukasbalz.business@gmail.com>
2025-04-24 18:00:43 +0200

    jsdoc for chicken class

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,4 +1,12 @@
+/**
+ * Class representing a chicken enemy in the game
+ * @extends MovableObject
+ */
 class Chicken extends MovableObject {
+    /**
+     * Animates the chicken's movement and sprite animation
+     * @param {string[]} images - Array of image paths for the animation frames
+     */
     animate(images) {
         setInterval(() => {
             this.movingLeft();
@@ -13,6 +21,10 @@ class Chicken extends MovableObject {
         }, 250);
     }
 
+    /**
+     * Generates a random position for the chicken respawn
+     * @returns {number} Random position between 800 and 10800 pixels
+     */
     randomChickenPosition() {
         return 800 + Math.random() * 10000;
     }
