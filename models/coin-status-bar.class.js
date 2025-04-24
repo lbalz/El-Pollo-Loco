da762cb730c73822e050f451f295fdc855bee58b
Lukas Balz <lukasbalz.business@gmail.com>
2025-04-24 18:02:49 +0200

    jsdoc for coin-status-bar

--- models/coin-status-bar.class.js
+++ models/coin-status-bar.class.js
@@ -1,14 +1,28 @@
+/**
+ * Class representing the coin collection status bar in the game
+ * @extends StatusBar
+ */
 class CoinStatusBar extends StatusBar {
+    /** @type {number} Current percentage of coins collected */
     coinPercentage = 0;
+
+    /** 
+     * @type {string[]} Array of image paths for different coin status bar states
+     * Images represent coin collection states from 0 to 20 coins (0% to 100%)
+     */
     COINS_IMAGES = [
-        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/0.png', // 0 Coins
-        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/20.png', // 4 Coins
-        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/40.png', // 8 Coins
-        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/60.png', // 12 Coins
-        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/80.png', // 16 Coins
-        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/100.png', // 20 Coins
+        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/0.png',
+        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/20.png',
+        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/40.png',
+        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/60.png',
+        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/80.png',
+        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/100.png',
     ];
 
+    /**
+     * Creates a new coin status bar
+     * Initializes position, dimensions, and initial coin percentage
+     */
     constructor() {
         super();
         this.loadImage('./img/7_statusbars/1_statusbar/1_statusbar_coin/blue/0.png');
