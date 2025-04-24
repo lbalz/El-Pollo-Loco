baf626179249fd3d85ec17471bbcad650fc85d46
Lukas Balz <lukasbalz.business@gmail.com>
2025-04-24 18:24:11 +0200

    jsdoc for throwable-object class

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,5 +1,15 @@
+/**
+ * Class representing a throwable object (bottle) in the game
+ * Handles throwing physics and direction
+ * @extends MovableObject
+ */
 class ThrowableObject extends MovableObject {
-
+    /**
+     * Creates a new throwable object
+     * @param {number} posX - Initial X position of the throw
+     * @param {number} posY - Initial Y position of the throw
+     * @param {boolean} characterDirection - Direction of throw (true for left, false for right)
+     */
     constructor(posX, posY, characterDirection) {
         super();
         this.loadImage('../img/6_salsa_bottle/salsa_bottle.png');
@@ -15,6 +25,12 @@ class ThrowableObject extends MovableObject {
         
     }
 
+    /**
+     * Throws the bottle to the right
+     * Applies gravity and horizontal movement
+     * @param {number} posX - Starting X position
+     * @param {number} posY - Starting Y position
+     */
     throwBottleRight(posX, posY) {
         this.positionX = posX;
         this.positionY = posY;
@@ -25,6 +41,12 @@ class ThrowableObject extends MovableObject {
         }, 50);
     }
 
+    /**
+     * Throws the bottle to the left
+     * Applies gravity and horizontal movement
+     * @param {number} posX - Starting X position
+     * @param {number} posY - Starting Y position
+     */
     throwBottleLeft(posX, posY) {
         this.positionX = posX;
         this.positionY = posY;
