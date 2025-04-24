a4a71413038b74abf80c0e75a7da755525b08e63
Lukas Balz <lukasbalz.business@gmail.com>
2025-04-24 17:51:34 +0200

    jsdoc for bg objects

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,8 +1,19 @@
+/**
+ * Class representing a background object in the game
+ * @extends MovableObject
+ */
 class BackgroundObject extends MovableObject {
-
+    /** @type {number} Width of the background object in pixels */
     width = 1080;
+
+    /** @type {number} Height of the background object in pixels */
     height = 720;
 
+    /**
+     * Creates a new background object
+     * @param {string} imagePath - Path to the background image file
+     * @param {number} positionX - Initial X position of the background object
+     */
     constructor(imagePath, positionX) {
         super().loadImage(imagePath);
         this.positionX = positionX;
