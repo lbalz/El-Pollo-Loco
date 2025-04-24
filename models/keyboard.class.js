a74091ecba9b9f0ba7ad7fc987ae5eaca2633836
Lukas Balz <lukasbalz.business@gmail.com>
2025-04-24 18:15:28 +0200

    jsdoc for keyboard class

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -1,9 +1,22 @@
+/**
+ * Class representing the keyboard input state for game controls
+ */
 class Keyboard {
+    /** @type {boolean} Whether the left movement key is pressed */
     LEFT = false;
-    RIGHT = false;
-    UP = false;
-    DOWN = false;
-    SPACE = false;
-    THROW = false;
 
+    /** @type {boolean} Whether the right movement key is pressed */
+    RIGHT = false;
+
+    /** @type {boolean} Whether the up/jump key is pressed */
+    UP = false;
+
+    /** @type {boolean} Whether the down key is pressed */
+    DOWN = false;
+
+    /** @type {boolean} Whether the space bar is pressed */
+    SPACE = false;
+
+    /** @type {boolean} Whether the throw action key is pressed */
+    THROW = false;
 }
