88829a880a0b1283721dcd8238e1718478ef8d14
Lukas Balz <lukasbalz.business@gmail.com>
2025-04-24 18:14:38 +0200

    jsdoc for health-status-bar

--- models/health-status-bar.class.js
+++ models/health-status-bar.class.js
@@ -1,5 +1,15 @@
+/**
+ * Class representing the health status bar for the player character
+ * @extends StatusBar
+ */
 class HealthStatusBar extends StatusBar {
+    /** @type {number} Current health percentage of the character */
     healthPercentage = 0;
+
+    /** 
+     * @type {string[]} Array of image paths for different health bar states
+     * Images represent health states from 0% to 100% in 20% increments
+     */
     HEALTH_POINTS_IMAGES = [
         './img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png',
         './img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
@@ -9,6 +19,10 @@ class HealthStatusBar extends StatusBar {
         './img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png',
     ];
 
+    /**
+     * Creates a new health status bar
+     * Initializes position, dimensions, and sets initial health to 100%
+     */
     constructor() {
         super();
         this.loadImage('./img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png');
