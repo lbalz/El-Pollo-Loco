40f5db0a4b7b9cb8cc79450c2d55f3c3bb9a3e7d
Lukas Balz <lukasbalz.business@gmail.com>
2025-04-24 18:19:19 +0200

    jsdoc for movable-object

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,25 +1,64 @@
+/**
+ * Base class for all objects that can move in the game
+ * Handles movement, collision, gravity, and status effects
+ * @extends DrawableObject
+ */
 class MovableObject extends DrawableObject {
+    /** @type {number} Base movement speed of the object */
     movingSpeed = 0.25;
+
+    /** @type {boolean} Whether the object is facing the opposite direction */
     otherDirection = false;
+
+    /** @type {number} Vertical speed/velocity of the object */
     speedPosY = 0;
-    characterAcceleration = 2.5; // Beschleunigung
+
+    /** @type {number} Gravity/acceleration applied to the object */
+    characterAcceleration = 2.5;
+
+    /** @type {number} Current health points of the object */
     healthPoints = 100;
+
+    /** @type {number} Timestamp of the last hit taken */
     lastHit = 0;
+
+    /** @type {number} Number of coins collected */
     coins = 0;
+
+    /** @type {number} Timestamp of the last coin collected */
     lastCoinHit = 0;
+
+    /** @type {number} Number of bottles collected */
     bottles = 0;
+
+    /** @type {number} Timestamp of the last bottle collected */
     lastBottlesHit = 0;
+
+    /** @type {number} Health points of the end boss */
     endbossHealth = 250;
+
+    /** @type {number} Damage dealt by bottle hits */
     bottleDamage = 25;
 
+    /** @type {HTMLAudioElement} Sound effect for collecting coins */
     collectCoinAudio = new Audio('./audio/collect_coin.mp3');
+
+    /** @type {HTMLAudioElement} Sound effect for collecting bottles */
     collectBottleAudio = new Audio('./audio/collect_bottle.mp3');
 
+    /**
+     * Creates a new movable object
+     */
     constructor() {
         super();
     }
 
-
+    /**
+     * Checks if this object is colliding with another movable object
+     * Uses offset values for precise collision detection
+     * @param {MovableObject} movableObject - The object to check collision with
+     * @returns {boolean} True if objects are colliding
+     */
     isColliding(movableObject) {
         return this.positionX + this.width - this.offset.right > movableObject.positionX + movableObject.offset.left &&
             this.positionY + this.height - this.offset.bottom > movableObject.positionY + movableObject.offset.top &&
@@ -27,16 +66,25 @@ class MovableObject extends DrawableObject {
             this.positionY + this.offset.top < movableObject.positionY + movableObject.height - movableObject.offset.bottom;
     }
 
+    /**
+     * Handles coin collection and plays sound effect
+     */
     collectCoin() {
         this.collectCoinAudio.play();
         this.coins += 1;
     }
 
+    /**
+     * Handles bottle collection and plays sound effect
+     */
     collectBottle() {
         this.collectBottleAudio.play();
         this.bottles += 1;
     }
 
+    /**
+     * Applies damage to the object and updates last hit timestamp
+     */
     getHit() {
         this.healthPoints -= 5;
 
@@ -48,16 +96,28 @@ class MovableObject extends DrawableObject {
 
     }
 
+    /**
+     * Checks if the object is currently in hurt state
+     * @returns {boolean} True if object was hit within last 0.5 seconds
+     */
     isHurt() {
         let timePassed = new Date().getTime() - this.lastHit; // Difference in ms
         timePassed = timePassed / 1000; // Difference in s
         return timePassed < 0.5;
     }
 
+    /**
+     * Checks if the object has no health remaining
+     * @returns {boolean} True if health points are 0
+     */
     isDead() {
         return this.healthPoints == 0;
     }
 
+    /**
+     * Applies continuous gravity effect to the object
+     * Updates vertical position based on speed and acceleration
+     */
     applyGravity() {
         setInterval(() => {
             if (this.isNotOnGround() || this.speedPosY > 0) {
@@ -67,6 +127,10 @@ class MovableObject extends DrawableObject {
         }, 1000 / 25);
     }
 
+    /**
+     * Checks if object is above ground level
+     * @returns {boolean} True if object is in air
+     */
     isNotOnGround() {
         if (this instanceof ThrowableObject) { // Throwable Objects shoulds always falls
             return true;
@@ -75,6 +139,10 @@ class MovableObject extends DrawableObject {
         }
     }
 
+    /**
+     * Plays animation sequence from provided image array
+     * @param {string[]} images - Array of image paths for animation frames
+     */
     playAnimation(images) {
         let imagesIndex = this.currentImage % images.length;
         let imagePath = images[imagesIndex];
@@ -82,16 +150,26 @@ class MovableObject extends DrawableObject {
         this.currentImage++;
     }
 
+    /**
+     * Moves the object right if alive
+     */
     movingRight() {
         if (this.healthPoints <= 0) return;
         this.positionX += this.movingSpeed;
     }
 
+    /**
+     * Moves the object left if alive
+     */
     movingLeft() {
         if (this.healthPoints <= 0) return;
         this.positionX -= this.movingSpeed;
     }
 
+    /**
+     * Makes the object jump if alive
+     * Sets initial vertical speed
+     */
     jump() {
         if (this.healthPoints <= 0) return;
         this.speedPosY = 35;
