8a49411bc91e57a114c7c84ff65de94b23d1b45c
Lukas Balz <lukasbalz.business@gmail.com>
2025-04-24 18:04:17 +0200

    jsdoc for coin class

--- models/coin.class.js
+++ models/coin.class.js
@@ -1,9 +1,27 @@
+/**
+ * Class representing a collectible coin object in the game
+ * @extends DrawableObject
+ */
 class Coin extends DrawableObject {
+    /** @type {number} Initial horizontal position of the coin */
     positionX = 300;
+
+    /** @type {number} Initial vertical position of the coin */
     positionY = 500;
+
+    /** @type {number} Width of the coin sprite in pixels */
     width = 225;
+
+    /** @type {number} Height of the coin sprite in pixels */
     height = 225;
 
+    /** 
+     * @type {Object} Collision offset values for the coin
+     * @property {number} top - Top offset for collision detection
+     * @property {number} right - Right offset for collision detection
+     * @property {number} bottom - Bottom offset for collision detection
+     * @property {number} left - Left offset for collision detection
+     */
     offset = {
         top: 80,
         right: 80,
@@ -11,6 +29,11 @@ class Coin extends DrawableObject {
         left: 80
     };
 
+    /**
+     * Creates a new coin object at specified position
+     * @param {number} randomPosX - Random horizontal position for the coin
+     * @param {number} randomPosY - Random vertical position for the coin
+     */
     constructor(randomPosX, randomPosY) {
         super()
         this.loadImage('./img/8_coin/coin_1.png');
