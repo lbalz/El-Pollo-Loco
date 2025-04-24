3c64b5464e97bc9e8dab1f67561682263a8dca56
Lukas Balz <lukasbalz.business@gmail.com>
2025-04-24 17:54:24 +0200

    jsdoc for bottle-status-bar

--- models/bottle-status-bar.class.js
+++ models/bottle-status-bar.class.js
@@ -1,5 +1,15 @@
+/**
+ * Class representing the bottle collection status bar in the game
+ * @extends StatusBar
+ */
 class BottleStatusBar extends StatusBar {
+    /** @type {number} Current percentage of bottles collected */
     bottlePercentage = 0;
+
+    /** 
+     * @type {string[]} Array of image paths for different bottle status bar states
+     * Images represent 0%, 20%, 40%, 60%, 80%, and 100% states
+     */
     BOTTLE_IMAGES = [
         './img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png',
         './img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png',
@@ -9,6 +19,10 @@ class BottleStatusBar extends StatusBar {
         './img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png',
     ];
 
+    /**
+     * Creates a new bottle status bar
+     * Initializes position, dimensions, and initial bottle percentage
+     */
     constructor() {
         super();
         this.loadImage('./img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png');
