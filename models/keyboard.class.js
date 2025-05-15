c9fdd9eae3baae1c695754bc8d9129ec72148c21
Lukas Balz <lukasbalz.business@gmail.com>
2025-05-15 11:18:56 +0200

    updated keyboard.class.js jsdoc

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -1,22 +1,47 @@
 /**
  * Class representing the keyboard input state for game controls
+ * Manages the state of movement and action keys
  */
 class Keyboard {
-    /** @type {boolean} Whether the left movement key is pressed */
+    /** 
+     * @type {boolean} 
+     * @default false
+     * Whether the left movement key (LEFT ARROW or A) is pressed 
+     */
     LEFT = false;
 
-    /** @type {boolean} Whether the right movement key is pressed */
+    /** 
+     * @type {boolean} 
+     * @default false
+     * Whether the right movement key (RIGHT ARROW or D) is pressed 
+     */
     RIGHT = false;
 
-    /** @type {boolean} Whether the up/jump key is pressed */
+    /** 
+     * @type {boolean} 
+     * @default false
+     * Whether the up/jump key (UP ARROW or W) is pressed 
+     */
     UP = false;
 
-    /** @type {boolean} Whether the down key is pressed */
+    /** 
+     * @type {boolean} 
+     * @default false
+     * Whether the down key (DOWN ARROW or S) is pressed 
+     */
     DOWN = false;
 
-    /** @type {boolean} Whether the space bar is pressed */
+    /** 
+     * @type {boolean} 
+     * @default false
+     * Whether the space bar is pressed for jumping 
+     */
     SPACE = false;
 
-    /** @type {boolean} Whether the throw action key is pressed */
+    /** 
+     * @type {boolean} 
+     * @default false
+     * Whether the throw action key (E) is pressed for throwing bottles 
+     */
     THROW = false;
 }
