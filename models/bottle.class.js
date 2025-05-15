ce793b775398dcebb9b1a923d3e5f138c515c4ba
Lukas Balz <lukasbalz.business@gmail.com>
2025-05-15 10:56:34 +0200

    updated bottle.class.js jsdoc

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -1,22 +1,41 @@
 /**
  * Class representing a collectible bottle object in the game
+ * Bottles can be collected by the player and used as throwable weapons
  * @extends DrawableObject
  */
 class Bottle extends DrawableObject {
-    /** @type {number} Initial horizontal position of the bottle */
+    /** 
+     * @type {number} 
+     * @constant
+     * Initial horizontal position of the bottle in pixels
+     */
     positionX = 300;
 
-    /** @type {number} Vertical position of the bottle on ground */
+    /** 
+     * @type {number}
+     * @constant 
+     * Vertical position of the bottle on ground in pixels
+     */
     positionY = 550;
 
-    /** @type {number} Width of the bottle sprite in pixels */
+    /** 
+     * @type {number}
+     * @constant 
+     * Width of the bottle sprite in pixels
+     */
     width = 100;
 
-    /** @type {number} Height of the bottle sprite in pixels */
+    /** 
+     * @type {number}
+     * @constant 
+     * Height of the bottle sprite in pixels
+     */
     height = 100;
 
     /** 
-     * @type {Object} Collision offset values for the bottle
+     * @type {Object}
+     * @constant
+     * Collision offset values for the bottle's hitbox in pixels
      * @property {number} top - Top offset for collision detection
      * @property {number} right - Right offset for collision detection
      * @property {number} bottom - Bottom offset for collision detection
@@ -30,8 +49,10 @@ class Bottle extends DrawableObject {
     };
 
     /**
-     * Creates a new bottle object
-     * @param {number} randomPosX - Random horizontal position for the bottle
+     * Creates a new bottle object at a specified horizontal position
+     * @constructor
+     * @param {number} randomPosX - Random horizontal position for the bottle in pixels
+     * @throws {Error} If image loading fails
      */
     constructor(randomPosX) {
         super();
