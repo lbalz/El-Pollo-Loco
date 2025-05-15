d8f2f0eee3676fb8a8f959a6610d63bf6138fdb2
Lukas Balz <lukasbalz.business@gmail.com>
2025-05-15 11:03:17 +0200

    updated cloud.class.js jsdoc

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,20 +1,35 @@
 /**
  * Class representing a moving cloud in the game background
+ * Provides ambient background movement with continuous scrolling
  * @extends MovableObject
  */
 class Cloud extends MovableObject {
-    /** @type {number} Vertical position of the cloud */
+    /** 
+     * @type {number} 
+     * @constant
+     * Vertical position of the cloud from top of screen in pixels
+     */
     positionY = 20;
 
-    /** @type {number} Width of the cloud sprite in pixels */
+    /** 
+     * @type {number} 
+     * @constant
+     * Width of the cloud sprite in pixels
+     */
     width = 500;
 
-    /** @type {number} Height of the cloud sprite in pixels */
+    /** 
+     * @type {number} 
+     * @constant
+     * Height of the cloud sprite in pixels
+     */
     height = 250;
 
     /**
      * Creates a new cloud instance and initializes its position
      * Loads cloud image and starts movement animation
+     * @constructor
+     * @throws {Error} If image loading fails
      */
     constructor() {
         super().loadImage('./img/5_background/layers/4_clouds/1.png');
@@ -25,7 +40,8 @@ class Cloud extends MovableObject {
 
     /**
      * Starts the cloud's continuous left movement animation
-     * Updates position 60 times per second
+     * Updates position 60 times per second (60 FPS)
+     * @returns {void}
      */
     startAnimate() {
         setInterval(() => {
